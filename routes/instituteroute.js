8a6b2a2bb05f771aac56d5a34c5e37e7d1ca3ccc
prem913 <premchandmaddikuntla87@gmail.com>
2022-04-24 16:22:42 +0530

    institute login with email password added

--- routes/instituteroute.js
+++ routes/instituteroute.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { registerinstitute, institutelogin, instituteVerifyOtp } = require("../controllers/institutecontroller");
+const { registerinstitute, institutelogin } = require("../controllers/institutecontroller");
 
 router.post("/register", registerinstitute);
 router.post("/login",institutelogin);
-router.post("/verifyotp",instituteVerifyOtp);
 module.exports = router;
