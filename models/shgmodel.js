682785b3d0f2c1f04ffdf6a269fda421c9a06bb3
ayush <ayushgoswami.abr@gmail.com>
2022-06-12 04:39:26 +0530

    Added SHG report

--- models/shgmodel.js
+++ models/shgmodel.js
@@ -89,8 +89,60 @@ const shgSchema = new mongoose.Schema(
         },
       }
     ],
+    january: {
+      type: Number,
+      default: 0
+    },
+    february: {
+      type: Number,
+      default: 0
+    },
+    march: {
+      type: Number,
+      default: 0
+    },
+    april: {
+      type: Number,
+      default: 0
+    },
+    may: {
+      type: Number,
+      default: 0
+    },
+    june: {
+      type: Number,
+      default: 0
+    },
+    july: {
+      type: Number,
+      default: 0
+    },
+    august: {
+      type: Number,
+      default: 0
+    },
+    september: {
+      type: Number,
+      default: 0
+    },
+    october: {
+      type: Number,
+      default: 0
+    },
+    november: {
+      type: Number,
+      default: 0
+    },
+    december: {
+      type: Number,
+      default: 0
+    },
     otp: String,
     devicetoken: String,
+    totalrevenue: {
+      type: Number,
+      default: 0,
+    },
   },
   { timestamps: true }
 );
